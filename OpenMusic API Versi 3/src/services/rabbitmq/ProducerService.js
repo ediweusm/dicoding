1d6455b5a6d1aaa3d5b2cfe486f74efd0c5fc9d6
ediweusm <148738636+ediweusm@users.noreply.github.com>
2023-10-23 11:17:36 +0700

    Add files via upload

---
+++ OpenMusic API Versi 3/src/services/rabbitmq/ProducerService.js
@@ -0,0 +1,23 @@
+// @TODO-1: import amqp dari module amqplib
+const amqp = require('amqplib');
+const { config } = require('../../commons/config');
+
+const ProducerService = {
+  sendMessage: async (queue, message) => {
+    const connection = await amqp.connect(config.RABBITMQ_SERVER); // @TODO-2: buat koneksi ke rabbitmq. Referensi: https://www.dicoding.com/academies/271/tutorials/17629
+    const channel = await connection.createChannel();
+    await channel.assertQueue(queue, {
+      durable: true,
+    });
+
+    // @TODO-3: kirim pesan ke queue. Referensi: https://www.dicoding.com/academies/271/tutorials/17629
+    await channel.sendToQueue(queue, Buffer.from(message));
+
+    setTimeout(() => {
+      // @TODO-4: tutup koneksi ke rabbitmq. Referensi: https://www.dicoding.com/academies/271/tutorials/17629
+      connection.close();
+    }, 1000);
+  },
+};
+
+module.exports = ProducerService;
